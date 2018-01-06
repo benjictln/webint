92ea83eb2fbb32f38bc3ba03a3b2b99442d8dc9b
Benjamin Castellan <benjamincastellan@gmail.com>
2018-01-06 20:32:34 +0100

    video post added and possibility to change the url + set offset (q5)

--- Video.js
+++ Video.js
@@ -1,10 +1,16 @@
 function formVideoSubmit() {
-    var url = document.forms["videoForm"]["url_video"].value;
-    if (url == ""){
+    var my_url = document.forms["videoForm"]["url_video"].value;
+    if (my_url == ""){
         alert("Please enter a valid ulr");
         return;
     }
-    var video_avi = document.getElementById('video_src_avi');
+    var my_video = document.getElementById('video1');
+    var offset = document.forms["videoForm"]["offset"].value;
+
+    if (isNaN(offset)) offset = 0;
+    my_video.src = my_url + '#t=' + String(offset);
+    my_video.play();
+    /*var video_avi = document.getElementById('video_src_avi');
     var video_mp4 = document.getElementById('video_src_mp4');
     var video_ogv = document.getElementById('video_src_ogv');
 
@@ -12,6 +18,6 @@ function formVideoSubmit() {
     video_mp4.src='https://archive.org/download/summer-rapid-july2017/summer-rapid-240x350.mp4'
     video_ogv.src='https://archive.org/download/summer-rapid-july2017/summer-rapid-240x350.mp4'
 
-    document.getElementById('video1').play();
+    document.getElementById('video1').play();*/
 
 }
